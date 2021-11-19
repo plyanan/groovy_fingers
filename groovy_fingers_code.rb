490586dbe83a52d9d83b1834a96d377407d2870b
Ply Ananthothai <35329802+plyanan@users.noreply.github.com>
2021-11-19 09:00:13 +0700

    Add files via upload
    Code for the groovy finger soundtrack that is playable on the Sonic Pi software.

---
+++ groovy_fingers_code.rb
@@ -0,0 +1,39 @@
+live_loop :foo do
+  sample :loop_garzul
+  use_synth :prophet
+  play :c1, release: 8, cutoff: rrand(70, 130)
+  sleep 8
+  play :e2, release: 10, cutoff: rrand(70, 130)
+  sleep 8
+  play 50
+  sleep 1
+  play 36
+  play 48
+  sample :ambi_lunar_land
+  sample :ambi_drone
+  play_pattern_timed chord(:e1, :m13), 0.25
+end
+
+live_loop :bar do
+  sample :bd_haus
+  sleep 0.5
+end
+
+use_synth :tb303
+loop do
+  play choose(chord(:E2, :minor)), release: 0.35, cutoff: rrand(60, 120)
+  sleep 0.25
+end
+
+use_synth :tb303
+live_loop :squelch do
+  n = (ring :e1, :e2, :e3).tick
+  play n, release: 0.125, cutoff: 100, res: 0.8, wave: 0
+  sleep 0.125
+end
+
+live_loop :timbre do
+  use_synth (ring :tb303, :blade, :prophet, :saw, :beep, :tri).tick
+  play :e2, attack: 0, release: 0.5, cutoff: 100
+  sleep 0.5
+end
